2a9d696c500337ecb285b760f3ef290b95362742
Kory Prince <korylprince@gmail.com>
2014-10-16 09:39:46 -0500

    don't set owner as administrator may not exist.

--- mkpkg.sh
+++ mkpkg.sh
@@ -4,5 +4,5 @@ sudo rm -Rf /usr/share/java.installer
 sudo mkdir -p /usr/share/java.installer/
 sudo cp "$2" /usr/share/java.installer/java.pkg
 sudo pkggen -i com.oracle.java.installer  -v "$1" --postinstall scripts/postinstall files out.pkg
-sudo chown administrator:staff out.pkg
+chmod 666 out.pkg
 mv out.pkg "$3"
